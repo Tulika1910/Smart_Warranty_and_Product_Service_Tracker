78ef90217fe18bb634b7ff3e194ef8ca80e54062
Tulika Agrawal <71176769+Tulika1910@users.noreply.github.com>
2026-07-02 14:25:53 +0530

    Refactor database connection to use SQLAlchemy
    Replaced mysql.connector with SQLAlchemy for database connection and updated connection handling.

--- Capstone_Project/app/database_initialise.py
+++ Capstone_Project/app/database_initialise.py
@@ -1,27 +1,14 @@
-import mysql.connector
+from sqlalchemy import create_engine
 import streamlit as st
 
 def get_db_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="tulika",
-        database="smart_warranty_db"
-    )
-
-def import_csv_to_db(file_path, table_name):
-    conn = get_db_connection()
-    cursor = conn.cursor()
+    db_url = 'mysql+mysqlconnector://root:tulika@localhost/smart_warranty_db'
     
-    with open(file_path, 'r') as file:
-        import csv
-        reader = csv.reader(file)
-        next(reader) 
-        
-        query = f"INSERT INTO {table_name} VALUES (%s, %s, %s, %s, %s)"
-        for row in reader:
-            cursor.execute(query, row)
-            
-    conn.commit()
-    cursor.close()
-    conn.close()
+    try:
+        engine = create_engine(db_url)
+        with engine.connect() as connection:
+            print("Connection successful!")
+        return engine
+    except Exception as e:
+        st.error(f"Connection Failed: {e}")
+        return None
